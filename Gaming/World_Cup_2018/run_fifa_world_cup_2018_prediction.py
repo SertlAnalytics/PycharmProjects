71e6961ec2b3338f96785ad37ab44676b79e3a1b
Josef Sertl <26519550+JWSertl@users.noreply.github.com>
2018-06-14 15:06:30 +0200

    Forest Random für WM2018

--- Gaming/World_Cup_2018/run_fifa_world_cup_2018_prediction.py
+++ Gaming/World_Cup_2018/run_fifa_world_cup_2018_prediction.py
@@ -7,9 +7,14 @@ Date: 2018-06-13
 """
 
 import os
+import matplotlib.pyplot as plt
 from sertl_analytics.datafetcher.file_fetcher import FileFetcher
 import pandas as pd
 import numpy as np
+import math
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 
 
 class FC:
@@ -25,25 +30,33 @@ class FC:
     GOALS_AWAY = 'away_team/goals'
     HOME_PENALTIES = 'home_team/penalties'
     AWAY_PENALTIES = 'away_team/penalties'
+    HOME_TEAM_RANKING = 'home_team_ranking'
+    AWAY_TEAM_RANKING = 'away_team_ranking'
+    HOME_TEAM_RANKING_SQU = 'home_team_ranking_squared'
+    HOME_TEAM_RANKING_SQRT = 'home_team_ranking_sqrt'
+    AWAY_TEAM_RANKING_SQU = 'away_team_ranking_squared'
+    AWAY_TEAM_RANKING_SQRT = 'away_team_ranking_sqrt'
+    TEAM_RANKING_PRODUCT = 'team_ranking_product'
+    WINNER = 'winner_system'
 
 
-class WorldCupRankingModelApi:
+class WorldCupRankingAdjustmentApi:
     def __init__(self, actual_min_penalty = np.inf):
         self.actual_min_penalty = actual_min_penalty
         self.penalty = 0
         self.ranking_difference = 0
         self.ranking_reduction_factor = 0
-        self.ranking_enhancement_factors = 0
+        self.ranking_enhancement_factor = 0
         self.penalty_wrong_winner = 0
         self.penalty_wrong_remis = 0
         self.penalty_not_remis = 0
 
     def clone(self):
-        api = WorldCupRankingModelApi(self.actual_min_penalty)
+        api = WorldCupRankingAdjustmentApi(self.actual_min_penalty)
         api.penalty = self.penalty
         api.ranking_difference = self.ranking_difference
         api.ranking_reduction_factor = self.ranking_reduction_factor
-        api.ranking_enhancement_factors = self.ranking_enhancement_factors
+        api.ranking_enhancement_factor = self.ranking_enhancement_factor
         api.penalty_wrong_winner = self.penalty_wrong_winner
         api.penalty_wrong_remis = self.penalty_wrong_remis
         api.penalty_not_remis = self.penalty_not_remis
@@ -52,7 +65,7 @@ class WorldCupRankingModelApi:
     def print(self):
         print('Penalty: {:2.0f} for Ranking_difference: {} Reduction_factor: {:1.1f} Enhancement_factor: {:1.1f} '
               'Penalty_wrong_winner: {} Penalty_wrong_remis: {} Penalty_not_remis: {}'.format(
-            self.actual_min_penalty, self.ranking_difference, self.ranking_reduction_factor, self.ranking_enhancement_factors,
+            self.actual_min_penalty, self.ranking_difference, self.ranking_reduction_factor, self.ranking_enhancement_factor,
             self.penalty_wrong_winner, self.penalty_wrong_remis, self.penalty_not_remis)
         )
 
@@ -181,7 +194,7 @@ class WorldCupMatch:
                 self.goal_team_1_simulation = 1
                 self.goal_team_2_simulation = 2
 
-    def get_projected_winner(self, api: WorldCupRankingModelApi, for_simulation: bool):
+    def get_projected_winner(self, api: WorldCupRankingAdjustmentApi, for_simulation: bool):
         if self.status == 'completed' and for_simulation:
             return self.get_winner()
         if self.ranking_adjusted_difference >= api.ranking_difference:
@@ -189,17 +202,16 @@ class WorldCupMatch:
         else:
             return 0
 
-    def adjust_ranking_after_simulation(self, ranking_reduction_factor: float, ranking_enhancement_factor: float,
-                                        winner_simulation: int):
-        self.__adjust_ranking_for_winner__(ranking_reduction_factor, ranking_enhancement_factor, winner_simulation)
+    def adjust_ranking_after_simulation(self, api: WorldCupRankingAdjustmentApi, winner_simulation: int):
+        self.__adjust_ranking_for_winner__(api, winner_simulation)
 
-    def adjust_ranking(self, ranking_reduction_factor: float, ranking_enhancement_factor: float):
-        r_red = self.ranking_adjusted_difference * ranking_reduction_factor
-        r_enh = self.ranking_adjusted_difference * ranking_enhancement_factor
+    def adjust_ranking(self, api: WorldCupRankingAdjustmentApi):
         winner = self.get_winner()
-        self.__adjust_ranking_for_winner__(r_enh, r_red, winner)
+        self.__adjust_ranking_for_winner__(api, winner)
 
-    def __adjust_ranking_for_winner__(self, r_enh, r_red, winner):
+    def __adjust_ranking_for_winner__(self, api: WorldCupRankingAdjustmentApi, winner):
+        r_red = self.ranking_adjusted_difference * api.ranking_reduction_factor
+        r_enh = self.ranking_adjusted_difference * api.ranking_enhancement_factor
         if winner == 0:
             if self.team_1.ranking_adjusted < self.team_2.ranking_adjusted:
                 self.team_1.ranking_adjusted += r_enh / 2
@@ -242,26 +254,75 @@ class WorldCup:
     def __init__(self, year: int, host: str):
         self.year = year
         self.host = host
+        self.df_match = None
+        self.df_ranking = None
+        self.df_4_ml = None
         self.team_list = WorldCupTeamList()
         self.match_list = WorldCupMatchList()
         self.__init_lists__()
+        self._api_for_ranking_adjustments = self.__get_ranking_adjustment_parameters__()
+        self._api_for_ranking_adjustments.print()
+        self.__add_team_ranking_to_df_match__()
+        self.__create_df_4_ml__()
+        self.__print_statistics__()
+
+    @property
+    def api_for_ranking_adjustments(self):
+        return self._api_for_ranking_adjustments
+
+    @property
+    def first_open_match_number(self):
+        for number in self.match_list.index_list:
+            match = self.match_list.get_match(number)
+            if match.status == 'open':
+                return number
+        return 0
+
+    def plot_data(self):
+        xs = self.df_match[FC.HOME_TEAM_RANKING]
+        ys = self.df_match[FC.AWAY_TEAM_RANKING]
+        labels = self.df_match[FC.WINNER]
+        plt.scatter(xs, ys, c=labels)
+        plt.show()
+
+    def __print_statistics__(self):
         print('WorldCup statistics for {} ({}): Number Teams = {}, Number Matches with teams = {}'.format(
             self.year, self.host, self.team_list.length, self.match_list.length))
+
+    def __add_team_ranking_to_df_match__(self):
+        for ind, rows in self.df_match.iterrows():
+            if rows[FC.HOME_TEAM] not in ['', pd.np.nan]:
+                match = self.match_list.get_match(rows[FC.MATCH_NUMBER])
+                team_1 = self.team_list.get_team(rows[FC.HOME_TEAM])
+                team_2 = self.team_list.get_team(rows[FC.AWAY_TEAM])
+                self.df_match.loc[ind, FC.HOME_TEAM_RANKING] = round(team_1.ranking_adjusted, 1)
+                self.df_match.loc[ind, FC.AWAY_TEAM_RANKING] = round(team_2.ranking_adjusted, 1)
+                self.df_match.loc[ind, FC.HOME_TEAM_RANKING_SQU] = round(team_1.ranking_adjusted**2, 1)
+                self.df_match.loc[ind, FC.HOME_TEAM_RANKING_SQRT] = round(math.sqrt(team_1.ranking_adjusted), 1)
+                self.df_match.loc[ind, FC.AWAY_TEAM_RANKING_SQU] = round(team_2.ranking_adjusted**2, 1)
+                self.df_match.loc[ind, FC.AWAY_TEAM_RANKING_SQRT] = round(math.sqrt(team_2.ranking_adjusted), 1)
+                self.df_match.loc[ind, FC.TEAM_RANKING_PRODUCT] = \
+                    round(team_1.ranking_adjusted * team_2.ranking_adjusted, 1)
+                self.df_match.loc[ind, FC.WINNER] = match.get_winner()
+                match.adjust_ranking(self._api_for_ranking_adjustments)
+
+    def __create_df_4_ml__(self):
+        self.df_4_ml = self.df_match.loc[:, FC.HOME_TEAM_RANKING : FC.WINNER]
 
     def __init_lists__(self):
         if self.year == 2014:
             file_path = config.get_excel_2014_file_path()
-            df_match = FileFetcher(file_path, sheet_name=config.excel_2014_tabs[0]).df
-            df_ranking = FileFetcher(file_path, sheet_name=config.excel_2014_tabs[1]).df
+            self.df_match = FileFetcher(file_path, sheet_name=config.excel_2014_tabs[0]).df
+            self.df_ranking = FileFetcher(file_path, sheet_name=config.excel_2014_tabs[1]).df
         else:
             file_path = config.get_excel_2018_file_path()
-            df_match = FileFetcher(file_path, sheet_name=config.excel_2018_tabs[0]).df
-            df_ranking = FileFetcher(file_path, sheet_name=config.excel_2018_tabs[1]).df
-        self.__fill_team_list__(df_ranking)
-        self.__fill_match_list__(df_match)
+            self.df_match = FileFetcher(file_path, sheet_name=config.excel_2018_tabs[0]).df
+            self.df_ranking = FileFetcher(file_path, sheet_name=config.excel_2018_tabs[1]).df
+        self.__fill_team_list__()
+        self.__fill_match_list__()
 
-    def __fill_team_list__(self, df_team: pd.DataFrame):
-        for ind, row in df_team.iterrows():
+    def __fill_team_list__(self):
+        for ind, row in self.df_ranking.iterrows():
             team = WorldCupTeam(row[0], row[1])
             self.__adjust_official_ranking__(team)
             self.team_list.add_team(team)
@@ -269,51 +330,67 @@ class WorldCup:
     def __adjust_official_ranking__(self, team: WorldCupTeam):
         if team.name == self.host:
             team.ranking = round(team.ranking * 0.9, 2)
+            team.ranking_adjusted = team.ranking
 
-    def __fill_match_list__(self, df_match: pd.DataFrame):
-        for ind, row in df_match.iterrows():
+    def __fill_match_list__(self):
+        for ind, row in self.df_match.iterrows():
             if row[FC.HOME_TEAM] not in ['', pd.np.nan]:
                 match = WorldCupMatch(self.team_list, row)
                 self.match_list.add_match(match)
 
-    def get_ranking_model_parameters(self) -> WorldCupRankingModelApi:
-        ranking_diff = np.arange(1, 10, 1)
-        ranking_reduction_factors = np.arange(0, 1.1, 0.1)
-        ranking_enhancement_factors = np.arange(0, 1.1, 0.1)
+    def __get_ranking_adjustment_parameters__(self) -> WorldCupRankingAdjustmentApi:
+        pass
+
+    def simulate_model(self, api: WorldCupRankingAdjustmentApi):
+        print('Caution: Simulation is done only for matches in status "open".')
+        self.team_list.reset_ranking_adjusted()
+        for index in self.match_list.index_list:
+            match = self.match_list.get_match(index)
+            winner_by_model = match.get_projected_winner(api, True)
+            match.simulate_with_winner_by_model(winner_by_model)
+            match.adjust_ranking_after_simulation(api, winner_by_model)
+
+
+class WorldCup4Training(WorldCup):
+    def __get_ranking_adjustment_parameters__(self) -> WorldCupRankingAdjustmentApi:
+        ranking_diff = np.arange(10, 0, -1)
+        ranking_reduction_factors = np.arange(0.7, 0, -0.1)
+        ranking_enhancement_factors = np.arange(0.7, 0, -0.1)
         penalty_wrong_winner = [1, 2, 3]
         penalty_wrong_remis = [1, 2, 3]
         penalty_not_remis = [1, 2, 3]
 
         # ranking_diff = [1]
-        # ranking_reduction_factors = [1]
-        # ranking_enhancement_factors = [1]
+        # ranking_reduction_factors = [0.7]
+        # ranking_enhancement_factors = [0.2]
         # penalty_wrong_winner = [1]
         # penalty_wrong_remis = [1]
         # penalty_not_remis = [1]
 
-        api_penalty_min = WorldCupRankingModelApi(np.inf)
+        api_penalty_min = WorldCupRankingAdjustmentApi(np.inf)
         for diff in ranking_diff:
-            api = WorldCupRankingModelApi(api_penalty_min.actual_min_penalty)
+            api = WorldCupRankingAdjustmentApi(api_penalty_min.actual_min_penalty)
             api.ranking_difference = diff
             for r_factors in ranking_reduction_factors:
                 api.ranking_reduction_factor = r_factors
                 for e_factors in ranking_enhancement_factors:
-                    api.ranking_enhancement_factors = e_factors
+                    api.ranking_enhancement_factor = e_factors
                     for p_wrong_winner in penalty_wrong_winner:
                         api.penalty_wrong_winner = p_wrong_winner
                         for p_wrong_remis in penalty_wrong_remis:
                             api.penalty_wrong_remis = p_wrong_remis
                             for p_not_remis in penalty_not_remis:
                                 api.penalty_not_remis = p_not_remis
-                                self.__calculate_penalty_for_this_model__(api)
+                                self.__calculate_penalty_for_this_ranking_adjustment_model__(api)
                                 if api.penalty < api_penalty_min.actual_min_penalty:
                                     api.actual_min_penalty = api.penalty
                                     api_penalty_min = api.clone()
                                     # api_penalty_min.print()
                                     # self.team_list.print_list()
+        self.team_list.reset_ranking_adjusted()
         return api_penalty_min
 
-    def __calculate_penalty_for_this_model__(self, api: WorldCupRankingModelApi):
+    def __calculate_penalty_for_this_ranking_adjustment_model__(self, api: WorldCupRankingAdjustmentApi):
         api.penalty = 0
         self.team_list.reset_ranking_adjusted()
         for index in self.match_list.index_list:
@@ -321,7 +398,7 @@ class WorldCup:
             if match.status == 'completed':
                 winner = match.get_winner()
                 winner_by_model = match.get_projected_winner(api, False)
-                match.adjust_ranking(api.ranking_reduction_factor, api.ranking_enhancement_factors)
+                match.adjust_ranking(api)
                 if winner != winner_by_model:
                     if winner == 0:
                         api.penalty += api.penalty_not_remis
@@ -333,29 +410,66 @@ class WorldCup:
                 if api.penalty > api.actual_min_penalty:
                     return
 
-    def simulate_model(self, api: WorldCupRankingModelApi):
-        print('Caution: Simulation is done only for matches in status "open".')
-        self.team_list.reset_ranking_adjusted()
-        for index in self.match_list.index_list:
-            match = self.match_list.get_match(index)
-            winner_by_model = match.get_projected_winner(api, True)
-            match.simulate_with_winner_by_model(winner_by_model)
-            match.adjust_ranking_after_simulation(api.ranking_reduction_factor, api.ranking_enhancement_factors
-                                                  , winner_by_model)
+
+class WorldCup4Test(WorldCup):
+    def __init__(self, year: int, host: str, api: WorldCupRankingAdjustmentApi):
+        self._api_for_ranking_adjustments = api
+        WorldCup.__init__(self, year, host)
+
+    def __get_ranking_adjustment_parameters__(self) -> WorldCupRankingAdjustmentApi:
+        return self._api_for_ranking_adjustments
 
 
 class WorldCupModel:
     def __init__(self):
-        self.world_cup_2014 = WorldCup(2014, 'Brasil')
-        self.world_cup_2018 = WorldCup(2018, 'Russia')
+        self.world_cup_2014 = WorldCup4Training(2014, 'Brasil')
+        self.world_cup_2018 = WorldCup4Test(2018, 'Russia', self.world_cup_2014.api_for_ranking_adjustments)
+        self.df_train = self.world_cup_2014.df_4_ml
+        self.df_test = self.world_cup_2018.df_4_ml
+        self.__y_train = np.array(self.df_train.iloc[:, -1])
+        self.forest_clf = RandomForestClassifier(n_estimators=10)
 
-    def __calculate_ranking_model_for_2014__(self):
-        pass
+    def __get_column_start_end_with_best_score__(self) -> list:
+        col_start_end_list = [[FC.HOME_TEAM_RANKING, FC.AWAY_TEAM_RANKING]]
+        col_start_end_list.append([FC.HOME_TEAM_RANKING_SQU, FC.HOME_TEAM_RANKING_SQU])
+        col_start_end_list.append([FC.HOME_TEAM_RANKING_SQRT, FC.AWAY_TEAM_RANKING_SQRT])
+        col_start_end_list.append([FC.HOME_TEAM_RANKING, FC.AWAY_TEAM_RANKING_SQU])
+        col_start_end_list.append([FC.HOME_TEAM_RANKING_SQU, FC.AWAY_TEAM_RANKING_SQRT])
+        col_start_end_list.append([FC.HOME_TEAM_RANKING, FC.AWAY_TEAM_RANKING_SQRT])
+        score_high = 0
+        col_start_end = None
+        for cols in col_start_end_list:
+            x_train = np.array(self.df_train.loc[:, cols[0]:cols[1]])
+            score = cross_val_score(self.forest_clf, x_train, self.__y_train, cv=2, scoring="accuracy")
+            if score.mean() > score_high:
+                score_high = score.mean()
+                col_start_end = cols
+        print('Best columns with score.mean={}: {}'.format(score_high, col_start_end))
+        return col_start_end
+
+    def make_prediction_for_the_next_matches(self, matches: int = 5, over_train = False, overwrite_completed = False):
+        cols = self.__get_column_start_end_with_best_score__()
+        self.__train_on_old_data__(cols)
+        offset = 0 if (over_train or overwrite_completed) else self.world_cup_2018.first_open_match_number
+        if over_train:
+            x_test = np.array(self.df_train.loc[offset:offset + matches, cols[0]:cols[1]])
+        else:
+            x_test = np.array(self.df_test.loc[offset:offset + matches, cols[0]:cols[1]])
+        predict_probability = self.forest_clf.predict_proba(x_test)
+        print('...predict_probability:\n{}'.format(predict_probability.round(2)))
+        for number in range(offset + 1, offset + 1 + matches + 1):
+            if over_train:
+                match = self.world_cup_2014.match_list.get_match(number)
+            else:
+                match = self.world_cup_2018.match_list.get_match(number)
+            match.print()
+
+    def __train_on_old_data__(self, cols: list):
+        x_train = np.array(self.df_train.loc[:, cols[0]:cols[1]])
+        self.forest_clf.fit(x_train, self.__y_train)
 
 
 model = WorldCupModel()
-api = model.world_cup_2014.get_ranking_model_parameters()
-api.print()
-model.world_cup_2018.simulate_model(api)
-model.world_cup_2018.match_list.print_list()
+model.make_prediction_for_the_next_matches(10, True)
+
 
