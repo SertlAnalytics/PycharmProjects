c2aa021042b1ac88794430e75631aa85529df392
Josef Sertl <26519550+JWSertl@users.noreply.github.com>
2018-06-14 18:46:41 +0200

    WM2018 as online system (i.e. instantly updating)

--- Gaming/World_Cup_2018/run_fifa_world_cup_2018_prediction.py
+++ Gaming/World_Cup_2018/run_fifa_world_cup_2018_prediction.py
@@ -453,6 +453,7 @@ class WorldCupModel:
         self.df_train = self.world_cup_2014.df_4_ml
         self.df_test = self.world_cup_2018.df_4_ml
         self.__y_train = np.array(self.df_train.iloc[:, -1])
+        self.__y_test = np.array(self.df_test.iloc[:, -1])
         self.forest_clf = RandomForestClassifier(n_estimators=10, random_state=42)
 
     def __get_column_start_end_with_best_score__(self) -> list:
@@ -475,7 +476,7 @@ class WorldCupModel:
 
     def make_prediction_for_the_next_matches(self, matches: int = 5, over_train = False, overwrite_completed = False):
         cols = self.__get_column_start_end_with_best_score__()
-        self.__train_on_old_data__(cols)
+        self.__train_on_old_and_new_data__(cols)
         offset_number = 1 if (over_train or overwrite_completed) else self.world_cup_2018.first_open_match_number
         offset_index = offset_number - 1
         if over_train:
@@ -486,7 +487,7 @@ class WorldCupModel:
             df_test = self.df_test.loc[offset_index:offset_index + matches, cols[0]:cols[1]]
         predict_probability = self.forest_clf.predict_proba(np.array(df_test)).round(2)
         print(df_test.head(matches))
-        for number in range(offset_number, offset_number + matches + 1):
+        for number in range(offset_number, offset_number + matches):
             if over_train:
                 match = self.world_cup_2014.match_list.get_match(number)
             else:
@@ -494,8 +495,15 @@ class WorldCupModel:
             match.simulate_by_probabilities(predict_probability[number - offset_number])
             match.print()
 
-    def __train_on_old_data__(self, cols: list):
-        x_train = np.array(self.df_train.loc[:, cols[0]:cols[1]])
+    def __train_on_old_and_new_data__(self, cols: list):
+        df_train = self.df_train.loc[:, cols[0]:cols[1]]
+        if self.world_cup_2018.first_open_match_number > 1:
+            pos_right = self.world_cup_2018.first_open_match_number-2
+            df_test = self.df_test.loc[:pos_right, cols[0]:cols[1]]
+            df_train = df_train.append(df_test)
+            for k in range(0, pos_right + 1):
+                self.__y_train = np.append(self.__y_train, self.__y_test[k])
+        x_train = np.array(df_train)
         self.forest_clf.fit(x_train, self.__y_train)
 
 
